b06f9cee92d3eed0a0fa7148a6576e7bce4ec16d
Christopher Kallimachos <ckallimachos@gmail.com>
2024-12-10 20:29:02 +0200

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * doc comments / java doc
+ * Prints Hello World in standard output (console)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+       // Prints Hello World
+        /*
+         * Prints in standard output Hello World
+         * in the console
+         */
+        System.out.println("Hello World!");
+    }
+}

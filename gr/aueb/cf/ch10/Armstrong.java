223d6ef5fedbeea857a711ed16ceb80c83a9e37c
Christopher Kallimachos <ckallimachos@gmail.com>
2024-12-10 21:05:46 +0200

    armstrong class commit

--- gr/aueb/cf/ch10/Armstrong.java
+++ gr/aueb/cf/ch10/Armstrong.java
@@ -3,9 +3,8 @@ package gr.aueb.cf.ch10;
 import java.util.Scanner;
 
 /**
- * 153 = 1^3 + 5^3 + 3^3
+ * 153 => 1^3 + 5^3 + 3^3
  */
-
 public class Armstrong {
 
     public static void main(String[] args) {
@@ -13,10 +12,20 @@ public class Armstrong {
         int num;
         int sum = 0;
         int numberOfDigits;
+        int originalNum;
+        int digit;
+
+        System.out.println("Please enter a number (integer)");
+        num = scanner.nextInt();
+        numberOfDigits = String.valueOf(num).length();  // valueOf transforms a primitive to String
+        originalNum = num;
 
         while (num != 0) {
-
+            digit = num % 10;
+            sum += (int) Math.pow(digit, numberOfDigits);
+            num /= 10;          // num = num / 10;
         }
 
+        System.out.printf("%d%s Armstrong", originalNum, (sum == originalNum) ? " is" : " is not" );
     }
 }

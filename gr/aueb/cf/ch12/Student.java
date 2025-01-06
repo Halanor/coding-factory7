b4e7a1253f6cd67f13faddcd3c30c0af70c4cc83
Christopher Kallimachos <ckallimachos@gmail.com>
2025-01-06 16:42:37 +0200

    ch12

---
+++ gr/aueb/cf/ch12/Student.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch12;
+
+public class Student {
+    public static int id;
+    public static String firstname;
+    public static String lastname;
+}
---
+++ gr/aueb/cf/ch12/StudentDemo.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch12;
+
+public class StudentDemo {
+
+    public static void main(String[] args) {
+        Student alice = new Student();
+
+        Student.id = 2;
+        Student.firstname = "Alice";
+        Student.lastname = "Wonderland";
+        
+    }
+}

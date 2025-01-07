1a99cbc8654d7d0e9e38d0ab8a586b375e6cde0a
Christopher Kallimachos <ckallimachos@gmail.com>
2025-01-07 14:18:10 +0200

    Project 3 alt and update

--- gr/aueb/cf/exercises/Project03.java
+++ gr/aueb/cf/exercises/Project03.java
@@ -3,73 +3,57 @@ package gr.aueb.cf.exercises;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Project03 {
+
+    static char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    static int[] frequency = new int[26];
 
     public static void main(String[] args) {
 
         String fileName = "C:\\Users\\Christopher\\Documents\\Coding factory\\Java\\latin-symbols.txt";
 
-        // Create a 2D array with a fixed size to store characters and their frequencies.
-        Object[][] arr = new Object[100][2]; // Array to store at most 100 unique characters and their counts
-        int index = 0; // To keep track of the next available index in the array
-
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
                 for (char c : line.toCharArray()) {
-                    // Skip spaces
                     if (Character.isWhitespace(c)) {
                         continue; // Skip spaces
                     }
-
-                    // Check if the character is already in the array
-                    boolean found = false;
-
-                    for (int i = 0; i < index; i++) {
-                        if ((char) arr[i][0] == c) {
-                            arr[i][1] = (int) arr[i][1] + 1; // Increment the frequency
-                            found = true;
-                            break; // No need to continue searching
-                        }
-                    }
-
-                    // If character is not found, add it to the array
-                    if (!found) {
-                        arr[index][0] = c; // Store the character
-                        arr[index][1] = 1; // Initialize the frequency to 1
-                        index++;
-                    }
-
-                    // If the array is full, stop
-                    if (index >= arr.length) {
-                        break;
-                    }
+                    inputToFrequency(c);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        display();
+    }
 
-        for (int i = 0; i < index - 1; i++) {
-            for (int j = i + 1; j < index; j++) {
-                if ((int) arr[i][1] > (int) arr[j][1]) {
-                    // Swap rows if arr[i][1] > arr[j][1]
-                    Object[] temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+    public static int charToIndex(char input) {
+
+        for (int i = 0; i < 26; i++) {
+            if (input == alphabet[i]) {
+                return i;
             }
         }
+        return -1;
+    }
 
-        // Print out the populated array
-        for (int i = 0; i < index; i++) {
-            char character = (char) arr[i][0];
-            int frequency = (int) arr[i][1];
-            System.out.println("Character: " + character + ", Frequency: " + frequency);
+    public static void inputToFrequency(char input) {
+
+        int index = charToIndex(input);
+
+        if (index > 0) {
+            frequency[index] = frequency[index] + 1;
+        }
+    }
+
+    public static void display() {
+
+        for (int i = 0; i < 26; i++) {
+            if (frequency[i] > 0) {
+                System.out.printf("Letter " + alphabet[i] + " was found " + frequency[i] + " times\n");
+            }
         }
     }
 }
